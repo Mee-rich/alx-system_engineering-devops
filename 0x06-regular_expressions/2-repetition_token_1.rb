1224d5c7c88e10b7af4a6856f2053721ece72ce7
mee-rich <jamessuyi@gmail.com>
2024-02-27 15:44:11 +0100

    single line of code

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,5 +1,4 @@
 #!/usr/bin/env ruby
 # This script accepts one argument and pass it to
-# a regular epression matching method
-puts ARGV[0].scan(/hbtn/).join
-puts ARGV[0].scan(/hbn/).join
+# a regular expression matching method
+puts ARGV[0].scan(/hbt?n/).join
