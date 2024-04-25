f5a792bb5edcdd627cd9bf745e8866fe21bb4e81
Mee-rich <jamessuyi@gmail.com>
2024-04-24 21:27:29 -0700

    done

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,43 +1,34 @@
-# Puppet manifest to install nginx
-
-# Install Nginx
-package { 'nginx':
-	ensure => installed,
+# Install Nginx package
+package {'nginx':
+  ensure => 'present',
 }
 
-# Configure Nginx
-file_line { '/etc/nginx/sites-available/default':
-	ensure => present,
-	content => template('nginx/default.erb'),
-	require => Package['nginx'],
-	notify => Service['nginx'],
+# Update package list and install Nginx
+exec {'install nginx':
+  command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
+  provider => shell,
+  require => Package['nginx'],
 }
 
-# Ensure index.html file contains "Hello World!"
+# Create index.html with "Hello World!"
 file { '/var/www/html/index.html':
-	content => 'Hello World!',
-	require => Package['nginx'],
+  ensure  => file,
+  content => 'Hello World!',
+  require => Exec['install nginx'],
 }
 
-# Ensure Nginx service is running
+# Add redirection rule to Nginx configuration
+exec {'add redirection rule':
+  command  => 'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \\/redirect_me {\\n\\t\\treturn 301 https:\\/\\/blog.ehoneahobed.com\\/;\\n\\t}/" /etc/nginx/sites-available/default',
+  provider => shell,
+  require  => Package['nginx'],
+  notify   => Service['nginx'],
+}
+
+# Restart Nginx service
 service { 'nginx':
-  ensure => running,
-  enable => true,
-  require => Package['nginx'],
+  ensure  => running,
+  enable  => true,
+  require => [Exec['install nginx'], Exec['add redirection rule']],
 }
 
-
-# Template for Nginx default configuration
-# /etc/nginx/sites-available/default
-#
-# This template ensures Nginx listens on port 80,
-# returns "Hello World!" on the root path,
-# and performs a 301 redirection.
-
-# Define the server block
-file { '/etc/nginx/sites-available/default':
-  ensure => present,
-  content => template('nginx/default.erb'),
-  require => Package['nginx'],
-  notify => Service['nginx'],
-}
